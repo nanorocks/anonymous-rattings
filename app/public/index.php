9cb5dba16d586f82b6d4afade5fb0ba298067beb
Andrej Nankov <andrejnankov@gmail.com>
2022-08-03 17:39:18 +0200

    Add init repo

---
+++ app/public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . './../vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
